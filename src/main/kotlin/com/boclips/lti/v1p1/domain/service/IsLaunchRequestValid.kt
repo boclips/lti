326b0c4a84a363d5c30ca7b2a15fded78ef093c1
Rafal Lewandowski <rafal@boclips.com>
2019-06-06 11:49:11 +0100

    Add additional logging on launch request verification
    [#166417243]
    
    Signed-off-by: Matt Jones <matt@boclips.com>

--- src/main/kotlin/com/boclips/lti/v1p1/domain/service/IsLaunchRequestValid.kt
+++ src/main/kotlin/com/boclips/lti/v1p1/domain/service/IsLaunchRequestValid.kt
@@ -1,11 +1,23 @@
 package com.boclips.lti.v1p1.domain.service
 
+import mu.KLogging
 import org.imsglobal.lti.launch.LtiVerificationResult
 import org.springframework.stereotype.Service
 
 @Service
 class IsLaunchRequestValid {
+    companion object : KLogging()
+
     operator fun invoke(result: LtiVerificationResult): Boolean {
-        return result.success && result.ltiLaunchResult.resourceLinkId?.isNotBlank() ?: false
+        if (!result.success) {
+            logger.info { "LTI launch verification failed: ${result.error}" }
+            return false
+        }
+        if (result.ltiLaunchResult.resourceLinkId?.isNotBlank() != true) {
+            logger.info { "LTI resource link id was not provided" }
+            return false
+        }
+
+        return true
     }
 }
