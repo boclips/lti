9023bbf874c86e925f89e5ec695cf319bf65c4bf
Rafał Lewandowski <rafal_lewandowski@outlook.com>
2019-07-02 01:27:18 +0200

    Remove SameSite attribute from session cookies
    ...because that prevents opening our LTI pages in an iframe.
    
    [#166633271]

--- src/main/kotlin/com/boclips/lti/v1p1/configuration/HttpSessionConfig.kt
+++ src/main/kotlin/com/boclips/lti/v1p1/configuration/HttpSessionConfig.kt
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Profile
 import org.springframework.session.data.mongo.JacksonMongoSessionConverter
 import org.springframework.session.data.mongo.config.annotation.web.http.EnableMongoHttpSession
+import org.springframework.session.web.http.CookieSerializer
+import org.springframework.session.web.http.DefaultCookieSerializer
 
 @Profile("!test")
 @EnableMongoHttpSession(maxInactiveIntervalInSeconds = 3600)
@@ -11,5 +13,12 @@ class HttpSessionConfig {
     @Bean
     fun mongoSessionConverter(): JacksonMongoSessionConverter {
         return JacksonMongoSessionConverter()
+    }
+
+    @Bean
+    fun cookieSerializer(): CookieSerializer {
+        val cookieSerializer = DefaultCookieSerializer()
+        cookieSerializer.setSameSite(null)
+        return cookieSerializer
     }
 }
