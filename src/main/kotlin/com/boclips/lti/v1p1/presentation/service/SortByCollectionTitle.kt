ec996e8d8874aafd09bdaa08eb8377f65e7a8720
Rafal Lewandowski <rafal@boclips.com>
2019-08-06 13:43:28 +0200

    Sort collections containing numbers in titles according to numeric value
    A simplification I'm not too proud of to make Pearson's collections page
    look meaningfully.
    
    [#167616325]

--- src/main/kotlin/com/boclips/lti/v1p1/presentation/service/SortByCollectionTitle.kt
+++ src/main/kotlin/com/boclips/lti/v1p1/presentation/service/SortByCollectionTitle.kt
@@ -5,7 +5,20 @@ import org.springframework.stereotype.Service
 
 @Service
 class SortByCollectionTitle {
+    val titlesWithNumbersRegex = "[^\\d]*(\\d+)[^\\d]*".toRegex()
+
     operator fun invoke(collections: List<CollectionMetadata>): List<CollectionMetadata> {
+        val titlesWithNumbers: (CollectionMetadata) -> Boolean = { it.title.matches(titlesWithNumbersRegex) }
+
+        return sortItemsWithNumbers(collections.filter(titlesWithNumbers)) +
+            sortItemsWithoutNumbers(collections.filterNot(titlesWithNumbers))
+    }
+
+    private fun sortItemsWithNumbers(collections: List<CollectionMetadata>): List<CollectionMetadata> {
+        return collections.sortedBy { titlesWithNumbersRegex.find(it.title)!!.groupValues.component2().toInt() }
+    }
+
+    private fun sortItemsWithoutNumbers(collections: List<CollectionMetadata>): List<CollectionMetadata> {
         return collections.sortedBy { it.title.toLowerCase() }
     }
 }
--- src/test/kotlin/com/boclips/lti/v1p1/presentation/service/SortByCollectionTitleTest.kt
+++ src/test/kotlin/com/boclips/lti/v1p1/presentation/service/SortByCollectionTitleTest.kt
@@ -5,23 +5,56 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class SortByCollectionTitleTest {
-  @Test
-  fun `sorts collections by title`() {
-    val collections = listOf(
-      CollectionMetadata("D", "", "", emptyList()),
-      CollectionMetadata("a", "", "", emptyList()),
-      CollectionMetadata("c2", "", "", emptyList()),
-      CollectionMetadata("A", "", "", emptyList()),
-      CollectionMetadata("c1", "", "", emptyList()),
-      CollectionMetadata("z", "", "", emptyList())
-    )
+    @Test
+    fun `sorts collections alphabetically by title, ignoring case`() {
+        val collections = listOf(
+            CollectionMetadata("Doge", "", "", emptyList()),
+            CollectionMetadata("apple", "", "", emptyList()),
+            CollectionMetadata("car", "", "", emptyList()),
+            CollectionMetadata("Air", "", "", emptyList()),
+            CollectionMetadata("City", "", "", emptyList())
+        )
 
-    val sorted = sortByCollectionTitle(collections)
+        val sorted = sortByCollectionTitle(collections)
 
-    assertThat(sorted)
-      .extracting("title")
-      .containsExactly("a", "A", "c1", "c2", "D", "z")
-  }
+        assertThat(sorted)
+            .extracting("title")
+            .containsExactly("Air", "apple", "car", "City", "Doge")
+    }
 
-  val sortByCollectionTitle = SortByCollectionTitle()
+    @Test
+    fun `sorts titles with numbers according to the number`() {
+        val collections = listOf(
+            CollectionMetadata("Grade 11", "", "", emptyList()),
+            CollectionMetadata("Grade 10", "", "", emptyList()),
+            CollectionMetadata("Grade 9", "", "", emptyList()),
+            CollectionMetadata("Grade 66", "", "", emptyList()),
+            CollectionMetadata("Grade 1", "", "", emptyList())
+        )
+
+        val sorted = sortByCollectionTitle(collections)
+
+        assertThat(sorted)
+            .extracting("title")
+            .containsExactly("Grade 1", "Grade 9", "Grade 10", "Grade 11", "Grade 66")
+    }
+
+    @Test
+    fun `places titles with numbers before other ones`() {
+        val collections = listOf(
+            CollectionMetadata("Grade 11", "", "", emptyList()),
+            CollectionMetadata("Grade 2", "", "", emptyList()),
+            CollectionMetadata("Beagle", "", "", emptyList()),
+            CollectionMetadata("Apple", "", "", emptyList()),
+            CollectionMetadata("Grade 1", "", "", emptyList())
+        )
+
+        val sorted = sortByCollectionTitle(collections)
+
+        assertThat(sorted)
+            .extracting("title")
+            .containsExactly("Grade 1", "Grade 2", "Grade 11", "Apple", "Beagle")
+    }
+
+    val sortByCollectionTitle = SortByCollectionTitle()
 }
