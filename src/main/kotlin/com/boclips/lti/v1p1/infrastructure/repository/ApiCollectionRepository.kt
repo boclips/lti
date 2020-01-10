42a8764fb7d68ce36bc23138962d439d6885cd52
Rafał Lewandowski <rafal_lewandowski@outlook.com>
2020-01-10 11:49:50 +0100

    Use 'details' projections when fetching collections

--- src/main/kotlin/com/boclips/lti/v1p1/infrastructure/repository/ApiCollectionRepository.kt
+++ src/main/kotlin/com/boclips/lti/v1p1/infrastructure/repository/ApiCollectionRepository.kt
@@ -4,6 +4,8 @@ import com.boclips.lti.v1p1.domain.exception.ResourceNotFoundException
 import com.boclips.lti.v1p1.domain.model.Collection
 import com.boclips.lti.v1p1.domain.repository.CollectionRepository
 import com.boclips.videos.api.httpclient.CollectionsClient
+import com.boclips.videos.api.request.Projection
+import com.boclips.videos.api.request.collection.CollectionFilterRequest
 import feign.FeignException
 import org.springframework.stereotype.Repository
 
@@ -13,13 +15,19 @@ class ApiCollectionRepository(
 ) : CollectionRepository {
     override fun get(collectionId: String): Collection {
         try {
-            return CollectionResourceConverter.toCollection(collectionsClient.getCollection(collectionId))
+            return CollectionResourceConverter.toCollection(
+                collectionsClient.getCollection(
+                    collectionId = collectionId,
+                    projection = Projection.details
+                )
+            )
         } catch (exception: FeignException.NotFound) {
             throw ResourceNotFoundException("Collection with id $collectionId not found")
         }
     }
 
     override fun getMyCollections(): List<Collection> {
-        return collectionsClient.getCollections()._embedded.collections.map(CollectionResourceConverter::toCollection)
+        return collectionsClient.getCollections(CollectionFilterRequest(projection = Projection.details))
+            ._embedded.collections.map(CollectionResourceConverter::toCollection)
     }
 }
