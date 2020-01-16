9619887121399d1df5e61d803068cd772a779e51
Sebastian Kazenbroot-Guppy <sebastian@boclips.com>
2020-01-16 16:46:57 +0100

    Store user id on the session
    [#170026268]
    
    Signed-off-by: Rafal Lewandowski <rafal@boclips.com>

--- src/main/kotlin/com/boclips/lti/v1p1/domain/service/InitializeLtiSession.kt
+++ src/main/kotlin/com/boclips/lti/v1p1/domain/service/InitializeLtiSession.kt
@@ -1,21 +1,22 @@
 package com.boclips.lti.v1p1.domain.service
 
 import com.boclips.lti.v1p1.domain.model.CustomLaunchParams
-import mu.KLogging
 import org.springframework.stereotype.Service
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpSession
 
 @Service
 class InitializeLtiSession {
-    companion object : KLogging() {
+    companion object {
         const val authenticationStateHolder = "isAuthenticated"
         const val customLogoHolder = "customLogo"
+        const val userIdHolder = "userId"
     }
 
     operator fun invoke(request: HttpServletRequest, session: HttpSession) {
         markAsAuthenticated(session)
         handleLogo(request, session)
+        handleUserId(request, session)
     }
 
     private fun markAsAuthenticated(session: HttpSession) {
@@ -25,8 +26,14 @@ class InitializeLtiSession {
     private fun handleLogo(request: HttpServletRequest, session: HttpSession) {
         val customLogo: String = request.getParameter(CustomLaunchParams.LOGO).orEmpty()
         if (customLogo.isNotBlank()) {
-            logger.info { "custom_logo = $customLogo" }
             session.setAttribute(customLogoHolder, customLogo)
+        }
+    }
+
+    private fun handleUserId(request: HttpServletRequest, session: HttpSession) {
+        val userId: String = request.getParameter(CustomLaunchParams.USER_ID).orEmpty()
+        if (userId.isNotBlank()) {
+            session.setAttribute(userIdHolder, userId)
         }
     }
 }
--- src/test/kotlin/com/boclips/lti/v1p1/domain/service/InitializeLtiSessionTest.kt
+++ src/test/kotlin/com/boclips/lti/v1p1/domain/service/InitializeLtiSessionTest.kt
@@ -3,6 +3,8 @@ package com.boclips.lti.v1p1.domain.service
 import com.boclips.lti.v1p1.domain.model.CustomLaunchParams
 import com.boclips.lti.v1p1.domain.service.InitializeLtiSession.Companion.authenticationStateHolder
 import com.boclips.lti.v1p1.domain.service.InitializeLtiSession.Companion.customLogoHolder
+import com.boclips.lti.v1p1.domain.service.InitializeLtiSession.Companion.userIdHolder
+import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.whenever
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
@@ -16,54 +18,73 @@ import javax.servlet.http.HttpSession
 
 @ExtendWith(MockitoExtension::class)
 class InitializeLtiSessionTest {
-  @Test
-  fun `marks the session as authenticated`(@Mock request: HttpServletRequest) {
-    initializeLtiSession(request, session)
+    @Test
+    fun `marks the session as authenticated`(@Mock request: HttpServletRequest) {
+        initializeLtiSession(request, session)
 
-    assertThat(session.getAttribute(authenticationStateHolder)).isEqualTo(true)
-  }
+        assertThat(session.getAttribute(authenticationStateHolder)).isEqualTo(true)
+    }
 
-  @Test
-  fun `sets custom logo if one is provided in the request`(@Mock request: HttpServletRequest) {
-    val logoUri = "https://imagez.net/custom/logo.png"
-    whenever(request.getParameter(CustomLaunchParams.LOGO)).thenReturn(logoUri)
+    @Test
+    fun `sets custom logo if one is provided in the request`(@Mock request: HttpServletRequest) {
+        val logoUri = "https://imagez.net/custom/logo.png"
+        whenever(request.getParameter(CustomLaunchParams.LOGO)).thenReturn(logoUri)
 
-    initializeLtiSession(request, session)
+        initializeLtiSession(request, session)
 
-    assertThat(session.getAttribute(customLogoHolder)).isEqualTo(logoUri)
-  }
+        assertThat(session.getAttribute(customLogoHolder)).isEqualTo(logoUri)
+    }
 
-  @Test
-  fun `does not set custom logo if empty string is provided`(@Mock request: HttpServletRequest) {
-    whenever(request.getParameter(CustomLaunchParams.LOGO)).thenReturn("")
+    @Test
+    fun `does not set custom logo if empty string is provided`(@Mock request: HttpServletRequest) {
+        whenever(request.getParameter(CustomLaunchParams.LOGO)).thenReturn("")
 
-    initializeLtiSession(request, session)
+        initializeLtiSession(request, session)
 
-    assertThat(session.getAttribute(customLogoHolder)).isNull()
-  }
+        assertThat(session.getAttribute(customLogoHolder)).isNull()
+    }
 
-  @Test
-  fun `does not set custom logo if whitespace string is provided`(@Mock request: HttpServletRequest) {
-    whenever(request.getParameter(CustomLaunchParams.LOGO)).thenReturn("       ")
+    @Test
+    fun `does not set custom logo if whitespace string is provided`(@Mock request: HttpServletRequest) {
+        whenever(request.getParameter(CustomLaunchParams.LOGO)).thenReturn("       ")
 
-    initializeLtiSession(request, session)
+        initializeLtiSession(request, session)
 
-    assertThat(session.getAttribute(customLogoHolder)).isNull()
-  }
+        assertThat(session.getAttribute(customLogoHolder)).isNull()
+    }
 
-  @Test
-  fun `does not set custom logo if custom_logo parameter is not provided`(@Mock request: HttpServletRequest) {
-    initializeLtiSession(request, session)
+    @Test
+    fun `does not set custom logo if custom_logo parameter is not provided`(@Mock request: HttpServletRequest) {
+        initializeLtiSession(request, session)
 
-    assertThat(session.getAttribute(customLogoHolder)).isNull()
-  }
+        assertThat(session.getAttribute(customLogoHolder)).isNull()
+    }
 
-  val initializeLtiSession = InitializeLtiSession()
+    @Test
+    fun `sets user id on the session`(@Mock request: HttpServletRequest) {
+        val userId = "abc123"
+        whenever(request.getParameter(any())).thenReturn(userId)
 
-  lateinit var session: HttpSession
+        initializeLtiSession(request, session)
 
-  @BeforeEach
-  fun setUp() {
-    session = MockHttpSession()
-  }
+        assertThat(session.getAttribute(userIdHolder)).isEqualTo(userId)
+    }
+
+    @Test
+    fun `does not set user id on the session if it's not in the request`(@Mock request: HttpServletRequest) {
+        whenever(request.getParameter(any())).thenReturn(null)
+
+        initializeLtiSession(request, session)
+
+        assertThat(session.getAttribute(userIdHolder)).isEqualTo(null)
+    }
+
+    val initializeLtiSession = InitializeLtiSession()
+
+    lateinit var session: HttpSession
+
+    @BeforeEach
+    fun setUp() {
+        session = MockHttpSession()
+    }
 }
