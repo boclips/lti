339f680fdb86d1852dcc44caec23cdbe259d3fde
Boisababy Clips <bob@fleming.nowhere>
2020-03-23 18:21:59 +0100

    WIP

--- src/main/kotlin/com/boclips/lti/v1p3/presentation/LtiOnePointThreeLoginController.kt
+++ src/main/kotlin/com/boclips/lti/v1p3/presentation/LtiOnePointThreeLoginController.kt
@@ -1,24 +1,48 @@
 package com.boclips.lti.v1p3.presentation
 
+import com.boclips.lti.core.application.service.UriComponentsBuilderFactory
+import com.boclips.lti.v1p3.domain.model.SessionKeys
 import org.hibernate.validator.constraints.URL
 import org.springframework.stereotype.Controller
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.util.UriComponentsBuilder
+import java.util.UUID
+import javax.servlet.http.HttpSession
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
 
 @Validated
 @Controller
 @RequestMapping("/v1p3/login")
-class LtiOnePointThreeLoginController {
+class LtiOnePointThreeLoginController(private val uriComponentsBuilderFactory: UriComponentsBuilderFactory) {
     @PostMapping
     fun login(
-        @NotNull @URL @RequestParam("iss") issuer: String?,
+        @NotNull @URL(protocol = "https") @RequestParam("iss") issuer: String?,
         @NotBlank @RequestParam("login_hint") loginHint: String?,
-        @NotNull @URL @RequestParam("target_link_uri") targetLinkUri: String?
+        @NotNull @URL @RequestParam("target_link_uri") targetLinkUri: String?,
+        session: HttpSession
     ): String {
-        return "redirect:https://rafal.lewandowski"
+        val state = UUID.randomUUID().toString()
+        session.setAttribute(SessionKeys.state, state)
+
+        val authenticationRequestUri = UriComponentsBuilder.fromUriString(issuer!!)
+            .queryParam("scope", "openid")
+            .queryParam("response_type", "id_token")
+            .queryParam("client_id", "boclips")
+            .queryParam(
+                "redirect_uri",
+                uriComponentsBuilderFactory.getInstance().replacePath("/v1p3/auth").toUriString()
+            )
+            .queryParam("login_hint", loginHint!!)
+            .queryParam("state", state)
+            .queryParam("response_mode", "form_post")
+            .queryParam("nonce", UUID.randomUUID().toString())
+            .queryParam("prompt", "none")
+            .toUriString()
+
+        return "redirect:$authenticationRequestUri"
     }
 }
--- src/test/kotlin/com/boclips/lti/v1p3/presentation/LtiOnePointThreeLoginControllerIntegrationTest.kt
+++ src/test/kotlin/com/boclips/lti/v1p3/presentation/LtiOnePointThreeLoginControllerIntegrationTest.kt
@@ -1,6 +1,7 @@
 package com.boclips.lti.v1p3.presentation
 
 import com.boclips.lti.testsupport.AbstractSpringIntegrationTest
+import com.boclips.lti.v1p3.domain.model.SessionKeys
 import org.assertj.core.api.Assertions.assertThat
 import org.hamcrest.Matchers.containsString
 import org.junit.jupiter.api.Test
@@ -9,6 +10,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.request
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.web.util.UriComponentsBuilder
 import java.net.URI
 
 class LtiOnePointThreeLoginControllerIntegrationTest : AbstractSpringIntegrationTest() {
@@ -28,22 +30,34 @@ class LtiOnePointThreeLoginControllerIntegrationTest : AbstractSpringIntegration
                 val location = result.response.getHeader("Location")
                 val locationUri = URI(location!!)
 
+                // TODO I believe the value of iss parameter doesn't necessarily need to match the Platform authentication endpoint.
+                // We probably need to store a mapping on our side and retrieve the auth endpoint based on iss value.
                 assertThat(location).startsWith(iss)
+
                 assertThat(locationUri).hasParameter("scope", "openid")
                 assertThat(locationUri).hasParameter("response_type", "id_token")
                 assertThat(locationUri).hasParameter("client_id", "boclips")
                 assertThat(locationUri).hasParameter("redirect_uri", "http://localhost/v1p3/auth")
                 assertThat(locationUri).hasParameter("login_hint", loginHint)
-                assertThat(locationUri).hasParameter("state", result.request.session?.getAttribute("state") as? String)
+                assertThat(locationUri).hasParameter("state", result.request.session?.getAttribute(SessionKeys.state) as? String)
                 assertThat(locationUri).hasParameter("response_mode", "form_post")
-                assertThat(locationUri).hasParameter("nonce", result.request.session?.getAttribute("nonce") as? String)
+                val nonceParameter = UriComponentsBuilder.fromUri(locationUri).build().queryParams["nonce"]
+                assertThat(nonceParameter).isNotEmpty
                 assertThat(locationUri).hasParameter("prompt", "none")
             }
     }
 
     @Test
     fun `returns a bad request response when issuer is not a valid https URL`() {
-        TODO("Not yet implemented")
+        mvc.perform(
+            post("/v1p3/login")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("iss", "http://insecure.com")
+                .param("login_hint", "a-user-login-hint")
+                .param("target_link_uri", "https://tool.com/resource/super-cool")
+        )
+            .andExpect(status().isBadRequest)
+            .andExpect(content().string(containsString("issuer")))
     }
 
     @Test
