2f01a61b95969d137cc5062f170dde9a6d20e085
Remigiusz Oleszczuk <remy@boclips.com>
2020-06-01 12:51:58 +0200

    Eslint setup
    - config taken from teachers
    
    [#172144003]
    
    Signed-off-by: Rafal Lewandowski <rafal@boclips.com>

--- frontend/src/components/common/PageLayout.tsx
+++ frontend/src/components/common/PageLayout.tsx
@@ -1,28 +1,26 @@
 import { Layout, Row, Col } from 'antd';
 import React from 'react';
+import cx from 'classnames';
+import { Header } from '../../bits/components/header';
 import { TopNavbar } from './TopNavbar';
 import s from './pageLayout.module.less';
-import cx from 'classnames';
 
 interface Props {
   children: React.ReactNode;
 }
 
-const { Header, Content } = Layout;
-
-export const PageLayout = (props: Props): React.ReactElement => {
-  return (
+export const PageLayout = (props: Props): React.ReactElement => (
     <Layout>
-      <Header className={cx(s.header, s.content, s.headerFixed)}>
+      <Layout.Header className={cx(s.header, s.content, s.headerFixed)}>
         <TopNavbar />
-      </Header>
-      <Content className="page-layout page-layout__content  ">
+      </Layout.Header>
+      <Layout.Content className="page-layout page-layout__content  ">
         <Row>
           <Col sm={{ span: 24 }} md={{ span: 24 }}>
             {props.children}
           </Col>
+          <Header />
         </Row>
-      </Content>
+      </Layout.Content>
     </Layout>
   );
-};
