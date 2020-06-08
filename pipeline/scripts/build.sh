82bd645e743bd88bc4603776fc96078e8a58da69
Remigiusz Oleszczuk <remy@boclips.com>
2020-06-08 10:58:14 +0200

    build.sh update
    [#172144003]

--- pipeline/scripts/build.sh
+++ pipeline/scripts/build.sh
@@ -2,15 +2,15 @@
 
 set -eu
 
-export GRADLE_USER_HOME=".gradle"
-
-version=$(cat version/tag)
-
 echo "Adding bit.dev to npm registry"
 echo "always-auth=true" >> ~/.npmrc
 echo "@bit:registry=https://node.bit.dev" >> ~/.npmrc
 echo "//node.bit.dev/:_authToken={$BIT_TOKEN}" >> ~/.npmrc
 echo "Completed adding bit.dev to npm registry"
+
+export GRADLE_USER_HOME=".gradle"
+
+version=$(cat version/tag)
 
 (
 cd source
