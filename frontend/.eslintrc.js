243eb96859429234c0acc043d7ce75da67dd1aac
Remigiusz Oleszczuk <remy@boclips.com>
2020-07-02 12:11:30 +0200

    Add boclips eslint setup
    [#173170331]

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,37 +1,18 @@
 module.exports = {
-  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     project: './tsconfig.json',
-    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module', // Allows for the use of imports
+    ecmaVersion: 2020,
+    sourceType: 'module',
     ecmaFeatures: {
-      jsx: true, // Allows for the parsing of JSX
+      jsx: true,
     },
     createDefaultProgram: true,
   },
   settings: {
     react: {
-      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+      version: 'detect',
     },
   },
-  extends: ['airbnb-typescript', 'prettier/react'],
-  rules: {
-    'no-restricted-syntax': 'off',
-    'react/jsx-filename-extension': [
-      1,
-      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
-    ],
-    'react/prop-types': 0,
-    'implicit-arrow-linebreak': 'off',
-    'comma-dangle': 'off',
-    indent: 'off',
-    'no-trailing-spaces': 'off',
-    'function-paren-newline': 'off',
-    'no-param-reassign': ['error', { props: false }],
-    'operator-linebreak': 'off',
-    '@typescript-eslint/indent': ['error', 2],
-    'no-confusing-arrow': 'off',
-    'no-underscore-dangle': 'off',
-    'import/no-extraneous-dependencies': 'off',
-  },
+  extends: ['eslint-config-boclips'],
 };
