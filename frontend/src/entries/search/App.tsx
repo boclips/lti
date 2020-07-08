7698b7b12b7af7e99eb5834a78fa451f372b49fb
Matt Jones <matt@boclips.com>
2020-07-08 10:19:09 +0100

    Add add video button to search-and-embed

--- frontend/src/entries/search/App.tsx
+++ frontend/src/entries/search/App.tsx
@@ -1,22 +1,29 @@
 import React, { ReactElement } from 'react';
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-import { Provider } from 'react-redux';
+import { Video } from '@bit/boclips.boclips-ui.types.video';
+import { VideoCard } from '@bit/boclips.boclips-ui.components.video-card';
+import { Player } from 'boclips-player-react';
 import AuthService from '../../service/auth/AuthService';
-import LtiView from '../../views/LtiView';
+import SearchView from '../../views/searchView';
 import '../../index.less';
 
-const store = configureStore({
-  reducer: {},
-  middleware: [...getDefaultMiddleware()],
-});
+const renderVideoCard = (video: Video, loading: boolean) => (
+  <VideoCard
+    hideBadges
+    key={video.id}
+    video={video}
+    loading={loading}
+    authenticated
+    videoPlayer={
+      <Player videoUri={video.links?.self?.getOriginalLink()} />
+    }
+  />
+);
 
 const App = (): ReactElement => {
   AuthService.configureAxios();
 
   return (
-    <Provider store={store}>
-      <LtiView />
-    </Provider>
+    <SearchView renderVideoCard={renderVideoCard}/>  
   );
 };
 
--- frontend/src/entries/searchAndEmbed/App.tsx
+++ frontend/src/entries/searchAndEmbed/App.tsx
@@ -1,16 +1,30 @@
-import React from "react";
-import {Layout} from "antd";
-import HeaderWithLogo from '@bit/boclips.boclips-ui.components.header-with-logo';
-import Title from "antd/lib/typography/Title";
-import '../../index.less'
+import React from 'react';
+import '../../index.less';
+import { Video } from '@bit/boclips.boclips-ui.types.video';
+import { VideoCard } from '@bit/boclips.boclips-ui.components.video-card';
+import { Player } from 'boclips-player-react';
+import { Button } from 'antd';
+import SearchView from '../../views/searchView';
+import AuthService from '../../service/auth/AuthService';
 
-export const App = () => (
-  <Layout>
-    <HeaderWithLogo>
-      <div></div>
-    </HeaderWithLogo>
-    <Layout.Content>
-      <Title>HELLO - Search And Embed</Title>
-    </Layout.Content>
-  </Layout>
-)
+const renderVideoCard = (video: Video, loading: boolean) => (
+  <VideoCard
+    hideBadges
+    key={video.id}
+    video={video}
+    loading={loading}
+    videoActionButtons={<Button type="primary">+ Add to lesson</Button>}
+    authenticated
+    videoPlayer={
+      <Player videoUri={video.links?.self?.getOriginalLink()} />
+    }
+  />
+);
+
+const App = () => {
+  AuthService.configureAxios();
+
+  return <SearchView renderVideoCard={renderVideoCard}/>;
+};
+
+export default App;
