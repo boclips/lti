435a09ca6af33b81961f86da0b3a8a925dbb4c5a
Boisababy Clips <bob@fleming.nowhere>
2020-08-05 10:55:53 +0200

    wip

--- frontend/src/components/Select/index.tsx
+++ frontend/src/components/Select/index.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Button, Checkbox, Select } from 'antd';
 import { Facet } from 'boclips-api-client/dist/sub-clients/videos/model/VideoFacets';
-
 import s from './styles.module.less';
 
 interface Props {
@@ -11,47 +10,62 @@ interface Props {
 
 const SelectFilter = ({ options, onApply }: Props) => {
   const [selected, setSelected] = useState<string[]>([]);
-  const [selectedCount, setSelectedCount] = useState<number>(0);
+  const [showCount, setShowCount] = useState<number>();
+
   const values = Object.keys(options);
 
   const onClick = () => {
-    console.log('selected: ' + selected);
+    setShowCount(selected.length);
+    console.log(`selected: ${selected}`);
     onApply(selected);
   };
 
+  const getOptions = () =>
+    values.map((it) => ({
+      label: <Checkbox tabIndex={0} 
+        onClick={(e) => e.stopPropagation()} 
+        className={s.checkboxWrapper}>
+        {it}
+      </Checkbox>,
+      value: it,
+    }));
+
+  const manageSelected = (selectedValue: any) => {
+    const { value } = selectedValue;
+    if (selected.includes(value)) {
+      const newSelected = selected.filter((v) => v !== value);
+      setSelected(newSelected);
+    } else {
+      setSelected([...selected, value]);
+    }
+  };
+
   return (
-    <Select
-      onChange={(value) => {
-        console.log(value);
-      }}
-      showArrow
-      options={values.map((it) => ({
-        label: <Checkbox> {it} </Checkbox>,
-        value: it,
-      }))}
-      placeholder="Ages"
-      data-qa="select-dropdown"
-      className={s.selectWrapper}
-      labelInValue={true}
-      // value={{ value: '', label: <div>Ages: {selectedCount}</div> }}
-      mode="multiple"
-      showSearch={false}
-      onSelect={(e) => {
-        console.log(e);
-      }}
-      onClick={(e) => {
-        console.log(e);
-      }}
-      dropdownRender={(i) => {
-        return (
+    <>
+      <Select
+        showArrow
+        onChange={() => null}
+        options={getOptions()}
+        placeholder="Ages"
+        data-qa="select-dropdown"
+        className={s.selectWrapper}
+        dropdownClassName={s.filterSelectWrapper}
+        labelInValue
+        value={{ value: '', label: <div className={s.inputValueWrapper}> Ages {showCount && showCount} </div> }}
+        mode="multiple"
+        showSearch={false}
+        onSelect={(valueWithLabel) => {
+          manageSelected(valueWithLabel);
+        }}
+        dropdownRender={(i) => (
           <div>
             {i}
-            <hr />
+            <hr/>
             <Button onClick={onClick}> Apply </Button>
           </div>
-        );
-      }}
-    />
+        )}
+      />
+    </>
   );
 };
 
