fedee55eca391b4566a0a36978b77ae4c1f509e1
Mima Farley-Rose <mima@boclips.com>
2020-08-12 10:41:48 +0100

    Add no-results svg to no-results component
    [#173964729]

--- frontend/src/components/NoResults/NoResults.tsx
+++ frontend/src/components/NoResults/NoResults.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './NoResults.less';
+import NoResultsSVG from './no-results.svg';
 
 interface NoResultsProps{
   searchQuery:String;
@@ -8,6 +9,7 @@ interface NoResultsProps{
 const NoResults = ({ searchQuery }:NoResultsProps) => (
   <div
     className="no-results">
+    <NoResultsSVG/>
     <p className="no-results-text">{`We couldn't anything for "${searchQuery}" with your filters`}</p>
     <p className="no-results-description"> try again using different keywords or change the filters</p>
   </div>
