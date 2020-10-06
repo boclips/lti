b0b1a0bed2fee4ff86e2687beed349bc12d6fab7
Eva Varga <eva@boclips.com>
2020-10-06 08:44:35 +0100

    Remove bit setup from build and setup scripts
    [#174875942]

--- pipeline/scripts/build.sh
+++ pipeline/scripts/build.sh
@@ -2,12 +2,6 @@
 
 set -eu
 
-echo "Adding bit.dev to npm registry"
-echo "always-auth=true" >> ~/.npmrc
-echo "@bit:registry=https://node.bit.dev" >> ~/.npmrc
-echo "//node.bit.dev/:_authToken={$BIT_TOKEN}" >> ~/.npmrc
-echo "Completed adding bit.dev to npm registry"
-
 export GRADLE_USER_HOME=".gradle"
 
 version=$(cat version/tag)
