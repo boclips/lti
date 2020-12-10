a8144a51e72f825d81bb77823b3e42b74f1d06e5
Eva Varga <eva@boclips.com>
2020-12-10 16:43:26 +0000

    Use `maxOrNull` instead of deprecated `max`

--- src/main/kotlin/com/boclips/lti/v1p3/application/service/KeyPairService.kt
+++ src/main/kotlin/com/boclips/lti/v1p3/application/service/KeyPairService.kt
@@ -4,7 +4,7 @@ import com.boclips.lti.v1p3.application.model.RsaKeyPair
 
 class KeyPairService(private val keyPairs: List<RsaKeyPair>) {
     fun getLatestKeyPair(): RsaKeyPair {
-        return keyPairs.max() ?: throw IllegalStateException("No key pairs have been configured")
+        return keyPairs.maxOrNull() ?: throw IllegalStateException("No key pairs have been configured")
     }
 
     fun getAllKeyPairs(): List<RsaKeyPair> = keyPairs
