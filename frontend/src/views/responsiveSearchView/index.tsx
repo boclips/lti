f04b76929f5705fa5964bc1a9da535ea9ac7d453
Remigiusz Oleszczuk <remy@boclips.com>
2021-04-06 16:05:44 +0200

    Add basic css grid

--- frontend/src/views/responsiveSearchView/index.tsx
+++ frontend/src/views/responsiveSearchView/index.tsx
@@ -1,14 +1,22 @@
 import React, { useEffect } from 'react';
 import { useBoclipsClient } from '../../hooks/useBoclipsClient';
+import useFeatureFlags from '../../hooks/useFeatureFlags';
+import s from './style.module.less';
 
 const ResponsiveSearchView = () => {
   const client = useBoclipsClient();
+  const featureFlags = useFeatureFlags();
 
   useEffect(() => {
     client.subjects.getAll();
   }, [client.subjects]);
 
-  return <div> 123 </div>;
+  return (
+    <div className={`${s.grid} ${s.container}`}>
+      <div className={s.header}>123</div>
+      <div className={s.body}></div>
+    </div>
+  );
 };
 
 export default ResponsiveSearchView;
